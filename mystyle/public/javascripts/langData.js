ae518602c583e2e1ebe541b27bc8734661e60290
VolodumurGruv <gru.v@i.ua>
2021-04-26 03:12:56 +0300

    added language support

---
+++ mystyle/public/javascripts/langData.js
@@ -0,0 +1,64 @@
+const langData = {
+	lg: {
+		uk: {
+			title: "Портфоліо",
+			first: {
+				"first-h1": "Веб сайти на будь-який смак!!!",
+				"first-text1":
+					"Створення сучасних веб-сайтів згідно з Вашими вимогами. Із застосуванням",
+				"first-text2": "Також можуть бути використанні такі фреймворки як",
+				"first-text3":
+					"чи інші. Веб-сторінки будуть придатні до перегляду на будь-яких пристроях.",
+				"first-eng-text1": "HTML5, CSS3, ES6, Node JS, Angular9.",
+				"first-eng-text2": "Bootstrap, Materialize ",
+			},
+			popup: {
+				"popup-contact": "Залиште повідомлення",
+				"popup-name": "Ім'я",
+				"popup-email": "Електронна пошта",
+				"popup-send": "Відправити",
+				"popup-close": "Закрити",
+			},
+		},
+		ru: {
+			title: "Портфолио",
+			first: {
+				"first-h1": "Приветствую в моем портфолио",
+				"first-text1":
+					"Создание современных веб-сайтов согласно Ваших требований. С использованием ",
+				"first-text2": "Также могут быть использованы такие фреймоворки как",
+				"first-text3":
+					"или другие. Веб-страницы будут пригодны к просмотру на любых устройствах.",
+				"first-eng-text1": "HTML5, CSS3, ES6, Node JS, Angular9.",
+				"first-eng-text2": "Bootstrap, Materialize ",
+			},
+			popup: {
+				"popup-contact": "Оставьте сообщение",
+				"popup-name": "Имя",
+				"popup-email": "Електронная почта",
+				"popup-send": "Отправить",
+				"popup-close": "Закрыть",
+			},
+		},
+		en: {
+			title: "Portfolio",
+			first: {
+				"first-h1": "Welcome to my portfolio",
+				"first-text1":
+					"Creating modern websites according to your requirements. With the application",
+				"first-text2": " Such frameworks as ",
+				"first-text3":
+					"or others can also be used. Web pages will be viewable on any device",
+				"first-eng-text1": "HTML5, CSS3, ES6, Node JS, Angular9.",
+				"first-eng-text2": "Bootstrap, Materialize ",
+			},
+			popup: {
+				"popup-contact": "Contact me",
+				"popup-name": "Name",
+				"popup-email": "Email",
+				"popup-send": "Send",
+				"popup-close": "Close",
+			},
+		},
+	},
+};
